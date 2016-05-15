b4175eaa4f476af0f98658f8c37c09518a5ff194
时允 <shiyun@meituan.com>
2016-05-15 21:19:53 +0800

    添加样式

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SideBar from './SideBar';
 import hireInfo from './mocks/hireInfo';
+require("!style!css!less!./app.less");
 
 ReactDOM.render(
   <SideBar hireInfo={hireInfo} />,
